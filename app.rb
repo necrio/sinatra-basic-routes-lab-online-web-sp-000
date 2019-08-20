ab8b4b4af8993eec8afcd5ed2cbe79809d38527b
Keith Isham <kisham8899@gmail.com>
2019-08-20 11:58:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,14 +4,20 @@ class App < Sinatra::Base
   
   get '/name' do
   @names = Names.all
+  
+  erb :'medicines/index.html.erb'
 end
 
  get '/hometown' do
   @hometown = hometown.all
+  
+  erb :'medicines/index.html.erb'
 end
   
    get '/favorite-song' do
   @favorite-song= favorite-song.all
+  
+  erb :'medicines/index.html.erb'
 end
   
   
