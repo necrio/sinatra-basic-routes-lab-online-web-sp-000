d343fef3b9a3e1f7d2558a677ef9fa7a47ae0e25
Keith Isham <kisham8899@gmail.com>
2019-08-20 11:59:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,24 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/name' do
-  @names = Names.all
-  
-  erb :'medicines/index.html.erb'
-end
-
- get '/hometown' do
-  @hometown = hometown.all
-  
-  erb :'medicines/index.html.erb'
-end
-  
-   get '/favorite-song' do
-  @favorite-song= favorite-song.all
-  
-  erb :'medicines/index.html.erb'
-end
-  
+get '/'
   
   
   
